a1d776c6e87f2778910d88e1fc6fab8613224aab
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-06-14 15:00:38 -0500

    added some js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DomContentLoaded', () => {
-    
-})
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-btn')
+    const width = 10;
+});
